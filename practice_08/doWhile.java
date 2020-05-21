f56397eb4a0aa3ac2f096ef5359b4b4058ecaa7d
DipendraDLS <dipen.stha9860@hotmail.com>
2020-05-21 20:31:15 +0545

    doWhile and While loops.

---
+++ practice_08/doWhile.java
@@ -0,0 +1,11 @@
+package practice_8;
+
+public class doWhile {
+    public static void main(String[] args) {
+        int x = 1 ;
+        do {
+            System.out.println("Hello i am do while loop");
+            x++;
+        }while (x==0);
+    }
+}
---
+++ practice_08/whileStatement.java
@@ -0,0 +1,17 @@
+package practice_8;
+
+public class whileStatement {
+    public static void main(String[] args) {
+        int x = 0;
+        int  count = 0;
+
+        while (x<=10)
+        {
+            System.out.println("Hi I am loop");
+            x++;
+            count++;
+        }
+        System.out.println(count);
+
+    }
+}
