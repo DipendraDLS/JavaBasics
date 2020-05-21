5c1753b3897490e6dbceb883a83b31f8babf6d3f
DipendraDLS <dipen.stha9860@hotmail.com>
2020-05-21 20:29:37 +0545

    Increments.

---
+++ practice_05/increment.java
@@ -0,0 +1,12 @@
+package practice_5;
+
+public class increment {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(x++);
+        x= x+5;
+        System.out.println(x);
+
+
+    }
+}
