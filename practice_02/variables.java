83de4ebc6a076730d1e8fda6a2377afa6ec5175e
DipendraDLS <dipen.stha9860@hotmail.com>
2020-05-21 20:22:35 +0545

    Variables in Java.

---
+++ practice_02/variables.java
@@ -0,0 +1,21 @@
+package practice_2;
+
+public class variables {
+    public static void main (String[]args){
+        /* Data Types
+
+        byte(number, 1 byte)
+        short(number, 2 bytes)
+        int (number, 4 bytes)
+        long (number, 8 bytes)
+        float (float number, 4 bytes)
+        double(float number, 8 bytes)
+        char(a character, 2 bytes)
+        boolean( true or false, 1 byte)
+
+         */
+
+        int my_variables= 10;
+        System.out.println(my_variables);
+    }
+}
