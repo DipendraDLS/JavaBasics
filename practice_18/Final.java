a53e751fa6991a1f1b80c348676008a79a612649
DipendraDLS <dipen.stha9860@hotmail.com>
2020-05-21 20:39:04 +0545

    Final variables

---
+++ practice_18/Final.java
@@ -0,0 +1,14 @@
+package practice_18;
+
+public class Final {
+
+    public static void main(String[] args)
+    {
+        FinalVaraible obj  = new FinalVaraible();
+
+        //obj.number = 12;  // Final varaible lai  FinalVaraible vanni class mai initialize garsakyo
+                            //so yesari feri yeta pani  initialize garna paundaina.
+        System.out.println(obj.number);
+
+    }
+}
---
+++ practice_18/FinalVaraible.java
@@ -0,0 +1,11 @@
+package practice_18;
+
+public class FinalVaraible {
+
+    public final int number;
+
+    FinalVaraible()
+    {
+        number = 10;
+    }
+}
