1650ab2046c8915b28bbd309d531828567c5c966
DipendraDLS <dipen.stha9860@hotmail.com>
2020-05-21 20:27:54 +0545

    Math Airthmetics

---
+++ practice_04/Math_Arithmetic.java
@@ -0,0 +1,12 @@
+package practice_4;
+
+public class Math_Arithmetic {
+    public static void main(String[] args) {
+        int x, y, answer;
+        x= 5;
+        y=10;
+        answer = x + y;
+        System.out.println("Answer = " + answer);
+
+    }
+}
