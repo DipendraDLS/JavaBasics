2be3dbd05cb05a9e4a20458f38f48635e5ec9385
DipendraDLS <dipen.stha9860@hotmail.com>
2020-05-21 20:30:01 +0545

    Conditional Statements.

---
+++ practice_06/conditionalStatement.java
@@ -0,0 +1,24 @@
+package practice_6;
+
+public class conditionalStatement {
+    public static void main(String[] args) {
+        /*
+            if (boolean expression)
+            {
+            execute if the boolean expression is true.
+
+            }
+        */
+
+        int x = 20;
+        if (x==10)
+        {
+            System.out.println("Yes x = " + x);
+        }
+
+        else {
+            System.out.println("No x != " + 10);
+        }
+
+    }
+}
